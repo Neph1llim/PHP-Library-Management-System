ec598466841e8deb3ce814b1343a894fe2687ff2
Tazz <charlesaberdo@gmail.com>
2026-05-14 16:42:07 +0800

    feature: Add initial user data to the database

---
+++ database/temp.sql
@@ -0,0 +1,4 @@
+INSERT INTO users()
+VALUES
+    ('Admin', 'Admin', 'Admin', 'admin@admin.com', 'admin123', '1234567890', 'Admin Address', 'Admin', CURRENT_DATE),
+    ('User', 'One', 'Test', 'user@user.com', 'user123', '0987654321', 'User Address', 'User', CURRENT_DATE);
